c99b6eac045b061cb5a01cdefaab4af25f5d74be
sunz5010 <missyou_z@msn.com>
2018-04-30 17:01:28 +0800

    Create init-docker.sh

---
+++ init-docker.sh
@@ -0,0 +1,23 @@
+#! /bin/bash
+sudo apt-get remove docker docker-engine docker.io
+sudo apt-get update
+sudo apt-get install -y\
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo apt-key fingerprint 0EBFCD88
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+sudo apt-get update
+sudo apt-get install -y docker-ce
+
+# 注意
+# 注意，我這邊有特別開對外listen的port，這邊如果不是在private subnet底下的話會很危險
+# 注意
+sed -i "s/ExecStart=\/usr\/bin\/dockerd -H fd:\/\//ExecStart=\/usr\/bin\/dockerd -H fd:\/\/ -H tcp:\/\/0.0.0.0:2375/g" /lib/systemd/system/docker.service
+systemctl daemon-reload
+service docker restart
